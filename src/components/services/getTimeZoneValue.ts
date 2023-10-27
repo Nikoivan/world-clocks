4bca80c86886b53ac0088274752323140fb09319
Niko Ivan <nixonivan@rambler.ru>
2023-10-27 23:28:14 +0300

    bugs fixed

--- src/components/services/getTimeZoneValue.ts
+++ src/components/services/getTimeZoneValue.ts
@@ -1,28 +1,3 @@
-export default function getTimeZoneValue(arg: string): number {
-  const formattedArg = arg.toLowerCase();
-  if (formattedArg.includes("moscow") || formattedArg.includes("москва")) {
-    return 0;
-  } else if (
-    formattedArg.includes("warsaw") ||
-    formattedArg.includes("варшава")
-  ) {
-    return -1;
-  } else if (
-    formattedArg.includes("london") ||
-    formattedArg.includes("лондон")
-  ) {
-    return -2;
-  } else if (
-    formattedArg.includes("tbilisi") ||
-    formattedArg.includes("тбилиси")
-  ) {
-    return 1;
-  } else if (
-    formattedArg.includes("yekaterinburg") ||
-    formattedArg.includes("екатеринбург")
-  ) {
-    return 2;
-  } else {
-    return 0;
-  }
+export default function getTimeZoneValue(arg: number): number {
+  return Math.round(arg) % 12;
 }
